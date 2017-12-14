7631a88e3400f1f4e99aae7ae5a48cf95edd6ed2
Attila Gulyas <agulyas@societyfortheblind.org>
2017-12-14 10:23:07 -0800

    Initial Commit

---
+++ Access News Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Access News Reader
+//
+//  Created by Society for the Blind on 12/14/17.
+//  Copyright © 2017 Society for the Blind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
