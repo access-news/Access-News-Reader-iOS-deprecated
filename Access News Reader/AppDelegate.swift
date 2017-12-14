0010d27e5b2e1d9997d362bcc8b080436a766505
Attila Gulyas <agulyas@societyfortheblind.org>
2017-12-14 15:22:10 -0800

    Add FirebaseUI pod to Podfile

--- Access News Reader/AppDelegate.swift
+++ Access News Reader/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
 
         FirebaseApp.configure()
+
         return true
     }
 
