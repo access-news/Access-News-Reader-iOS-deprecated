4cf01af66143f09813e09991b5dfdf43e74cbbec
Attila Gulyas <agulyas@societyfortheblind.org>
2018-04-27 11:45:48 -0700

    Main: Clean up MainTVC and reshuffle in progress

--- Access News Reader/MainTableViewController.swift
+++ Access News Reader/MainTableViewController.swift
@@ -14,81 +14,8 @@ import UIKit
 
 class MainTableViewController: UITableViewController {
 
-    @IBOutlet weak var tooltips: UITextView!
-
-// TODO move this to wherever settings will be
-//
-//    @IBOutlet weak var changeEmailField: UITextField!
-//    @IBOutlet weak var submitEmailChange: UIButton!
-//    @IBAction func changeEmail(_ sender: Any) {
-//        
-//    }
-
-//    @IBOutlet weak var recordButton: UIBarButtonItem!
-//    @IBAction func recordTapped(_ sender: Any) {
-//        if self.audioRecorder == nil {
-//            self.setRecorder(publication: self.selectedPublication)
-//        }
-//        self.audioRecorder?.record()
-//
-//        self.updateControlsAndStatus(
-//            activeControls: [.stop],
-//            tooltipText:    nil,
-//            controlStatus:  ("Recording...", .red))
-//    }
-//
-//    @IBOutlet weak var stopButton: UIBarButtonItem!
-//    @IBAction func stopTapped(_ sender: Any) {
-//        let controlStatus: (String, UIColor)
-//
-//        if self.audioRecorder?.isRecording == true {
-//            self.audioRecorder?.stop()
-//            controlStatus = ("Recording stopped.", .red)
-//        } else {
-//            audioPlayer?.stop()
-//            controlStatus = ("Playback stopped.", .green)
-//        }
-//
-//        let tooltip =
-//            [ "Tap Record to continue recording."
-//            , "Tap Play to list to your recording so far."
-//            , "Tap Queue to add file to the list of recordings to be submitted later."
-//            , "Tap Submit to upload current file and queued recordings right now."
-//            ].joined(separator: "\n\n")
-//
-//        self.updateControlsAndStatus(
-//            activeControls: [.record, .play, .queue, .submit],
-//            tooltipText:    NSAttributedString(string: tooltip),
-//            controlStatus:  controlStatus)
-//    }
-//
-//    @IBOutlet weak var playButton: UIBarButtonItem!
-//    @IBAction func playTapped(_ sender: Any) {
-//        if self.audioPlayer == nil {
-//            self.setPlayer()
-//        }
-//        self.audioPlayer?.play()
-//
-//        self.updateControlsAndStatus(
-//            activeControls: [.stop],
-//            tooltipText:    NSAttributedString(string: "Tap Stop/Pause to halt playback."),
-//            controlStatus:  ("Playing recording...", .green))
-//    }
-//
-//    @IBOutlet weak var queueButton: UIBarButtonItem!
-//    @IBAction func queueTapped(_ sender: Any) {
-//        // Tap Record if you would like to read another article from <publication>
-//        // Or choose another publication to record from another
-//        self.resetAudioInstances()
-//    }
-//
-//    @IBOutlet weak var submitButton: UIBarButtonItem!
-//    @IBAction func submitButton(_ sender: Any) {
-//    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tooltips.isEditable = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -101,10 +28,6 @@ class MainTableViewController: UITableViewController {
     */	
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.tableView.deselectRow(at: indexPath, animated: false)
-
-//        if self.tableView.visibleCells[0].isSelected {
-//            self.navigationController?.pushViewController(SelectPublication(), animated: true)
-//        }
     }
     /*
     // MARK: - Navigation
